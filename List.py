9121f7dca472223c51a6529d86c1773af299b368
Viresh <kariappavbk123@gmail.com>
2025-12-12 11:16:07 +0530

    first commit

---
+++ List.py
@@ -0,0 +1,16 @@
+# Creating a list
+students = ["aditya","rahul","tejas","shivam","rohit","tejas"]
+print(students)
+
+# length function
+students = ["aditya","rahul","tejas","shivam","rohit","tejas"]
+print(len(students))
+
+# Accessing List Items
+students = ["aditya","rahul","tejas","shivam","rohit","tejas"]
+
+print(students[1])
+# range of indexes
+print(students[1:4])
+print(students[:4])
+print(students[2:])
